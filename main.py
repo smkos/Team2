3ec6b72be3edd6e1aceba8c74fea2c6a78093bef
Jiyoung <americanoshot5@gmail.com>
2026-06-30 16:11:37 +0900

    ADD getSum func

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pytest
 
 class Calc:
-    pass
+    def __init__(self):
+        pass
 
-def test_sample():
-    assert 1 == 1
-    pytest.fail()
+    def getSum(self, a, b):
+        return a + b
+
+def test_getSum():
+    calc = Calc()
+    ret = calc.getSum(1, 2)
+    assert ret == 3
