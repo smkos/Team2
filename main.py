a1afe90f2ce0503dcc1fb0048e1d89f27d7af6e8
Sangmin Kim <ps24663@gmail.com>
2026-06-30 16:12:25 +0900

    getDivide & getSumSum

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pytest
 
 class Calc:
-    pass
+    def getDivide(self, a, b):
+        return a / b
+
+    def getSumSum(self, a, b, c):
+        return a + b + c
 
 def test_sample():
     assert 1 == 1
